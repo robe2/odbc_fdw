945d1e89ee6b309daf2ccc184b1651306dc71937
Javier Goizueta <jgoizueta@gmail.com>
2020-02-17 14:12:25 +0100

    Prepare next release 0.5.1

---
+++ odbc_fdw--0.5.0--0.5.1.sql
@@ -0,0 +1,15 @@
+/*-------------------------------------------------------------------------
+ *
+ *                foreign-data wrapper for ODBC
+ *
+ * Copyright (c) 2011, PostgreSQL Global Development Group
+ * Copyright (c) 2016-2020 CARTO
+ *
+ * This software is released under the PostgreSQL Licence
+ *
+ * Original author: Zheng Yang <zhengyang4k@gmail.com>
+ *
+ *-------------------------------------------------------------------------
+ */
+
+-- Empty file as there are no changes in API between 0.5.0 and 0.5.1
