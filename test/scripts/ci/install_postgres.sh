bece47c1e37d2614fb5f189f25b38aa010b0ad7d
Javier Goizueta <jgoizueta@gmail.com>
2020-09-08 16:41:42 +0200

    Fix duplicated command

--- test/scripts/ci/install_postgres.sh
+++ test/scripts/ci/install_postgres.sh
@@ -16,7 +16,7 @@ apt-get install -y --allow-unauthenticated --no-install-recommends --no-install-
 # Recreate the cluster with the config we need
 for i in $(pg_lsclusters  | tail -n +2 | awk '{print $1}'); do pg_dropcluster --stop $i main; done;
 rm -rf /etc/postgresql/$POSTGRESQL_VERSION /var/lib/postgresql/$POSTGRESQL_VERSION	rm -rf /etc/postgresql/$POSTGRESQL_VERSION /var/lib/postgresql/$POSTGRESQL_VERSION /var/ramfs/postgresql/$POSTGRESQL_VERSION
-pg_createcluster -u postgres --locale C $POSTGRESQL_VERSION main -- -A trust	pg_createcluster -u postgres --locale C $POSTGRESQL_VERSION main --start -p 5432 -- -A trust
+pg_createcluster -u postgres --locale C $POSTGRESQL_VERSION main --start -p 5432 -- -A trust
 
 # Start the service
 /etc/init.d/postgresql start $POSTGRESQL_VERSION || sudo journalctl -xe
